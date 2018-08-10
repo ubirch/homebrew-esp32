7f725b6dc9ff7259e9db31ac1212b27e094466fe
Philipp Weißmann <mail@philipp-weissmann.de>
2018-08-10 16:02:29 +0200

    Fix esp-idf

--- esp-idf.rb
+++ esp-idf.rb
@@ -1,13 +1,10 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class EspIdf < Formula
   desc "ESP-IDF"
   homepage "https://github.com/espressif/esp-idf"
-  url "https://github.com/espressif/esp-idf.git", :branch => :master
+  url "https://github.com/espressif/esp-idf/releases/download/v3.0.3/esp-idf-v3.0.3.zip"
+  sha256 "b92d0ffe24f5b94666f56e438f9dce642bc9eec6e34988049fea9cab60a06afd"
   depends_on "xtensa-esp32-elf"
-  # depends_on "python@2" => ['pyserial']
+  depends_on "python"
 
   def install
     ENV.deparallelize
@@ -20,6 +17,8 @@ class EspIdf < Formula
   def caveats; <<~EOS
     You may wish to add the ESP-IDF environment (.profile, .bash_profile):
       export IDF_PATH=#{opt_prefix}
+
+    Also: If you want to flash, install the python package 'pyserial'
     EOS
   end
 
